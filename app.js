70e2f2f74175a76a798c0614c217574fc254c9dc
ConnorEndsley <endsleyconnor95@gmail.com>
2022-07-15 08:16:38 -0500

    first kermit

---
+++ app.js
@@ -0,0 +1,36 @@
+// items
+
+const menu = [
+    {
+        id: 1,
+        title: "pasta",
+        category: 'dinner',
+        price: 15.00,
+        img: './pasta.jpg',
+        description: 'Al dente pasta in a tomato basil sauce topped with parmesan cheese',
+    },
+
+    {
+        id: 2,
+        title: "noodles",
+        category: 'dinner',
+        price: 15.00,
+        img: './pasta.jpg',
+        description: 'Al dente pasta in a tomato basil sauce topped with parmesan cheese',
+    },
+
+    {
+        id: 3,
+        title: "pizza",
+        category: 'dinner',
+        price: 15.00,
+        img: './pasta.jpg',
+        description: 'Al dente pasta in a tomato basil sauce topped with parmesan cheese',
+    }
+]
+
+const sectionCenter = document.querySelector('section-center');
+
+window.addEventListener('DOMContentLoaded', function() {
+    console.log('working')
+})
